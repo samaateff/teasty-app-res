c8de1c13b7b530026672c74e4ba2985bdc5f6aaf
samaateff <71303025+samaateff@users.noreply.github.com>
2024-07-26 04:53:12 +0300

    new 3

--- src/components/UserNav.jsx
+++ src/components/UserNav.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import UserIcon from './icons/UserIcon';
 import styles from './navbar.module.css';
 
 function UserNav() {
+	const [droped , setDroped] = useState(false)
 	return (
-		<div className={styles.userIcons}>
+		<div className={styles.userIcons} onMouseEnter={() => setDroped(true)} onMouseLeave={() => setDroped(false)}>
 			<UserIcon />
+			{droped && <div className={styles.userMenu}>
+				<ul >
+				<li>
+					<div>
+						<img src='/Rectangle157.jpg'/>
+						<span>MY PROFILE</span>
+					</div>
+					<div>
+					&rarr;
+					</div>
+				</li>
+				<li>2</li>
+				<li>3</li>
+				</ul>
+			</div>
+			}
 		</div>
 	);
 }
