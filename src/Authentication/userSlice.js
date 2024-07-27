e30f3d5da4af8c2ddd9c7919b0a460a4af643d94
samaateff <71303025+samaateff@users.noreply.github.com>
2024-07-27 05:39:49 +0300

    new 3

--- src/Authentication/userSlice.js
+++ src/Authentication/userSlice.js
@@ -1,60 +1,65 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const fakeUser = {
-	id: 1,
-	name: 'sama',
-	email: 'sama@example.com',
-	password: '1234',
-	cart: [
-		{
-			id: 512,
-			name: 'Pizza margrita',
-			img: '/Rectangle157.jpg',
-			price: 10.99,
-			quantity: 2,
-		},
-		{
-			id: 513,
-			name: 'cheese burger',
-			img: '/Rectangle157.jpg',
-			price: 11.99,
-			quantity: 3,
-		},
-	],
-	favourites: [
-		{
-			id: 512,
-			name: 'Pizza margrita',
-		},
-		{
-			id: 513,
-			name: 'cheese burger',
-		},
-		{
-			id: 514,
-			name: 'steak',
-		},
-	],
+  id: 1,
+  name: 'sama',
+  email: 'sama@example.com',
+  password: '1234',
+  cart: [
+    {
+      id: 512,
+      name: 'Pizza margrita',
+      img: '/Rectangle157.jpg',
+      price: 10.99,
+      discount: 2,
+      quantity: 2,
+    },
+    {
+      id: 513,
+      name: 'cheese burger',
+      img: '/Rectangle157.jpg',
+      price: 11.99,
+      discount: 2,
+      quantity: 3,
+    },
+  ],
+  favourites: [
+    {
+      id: 512,
+      name: 'Pizza margrita',
+    },
+    {
+      id: 513,
+      name: 'cheese burger',
+    },
+    {
+      id: 514,
+      name: 'steak',
+    },
+  ],
 };
 const initialState = {
-	user: null,
-	isAuthenticated: false,
+  user: null,
+  isAuthenticated: false,
 };
 const userSlice = createSlice({
-	name: 'user',
-	initialState: initialState,
-	reducers: {
-		login(state, action) {
-			if (action.payload.email === fakeUser.email && action.payload.password === fakeUser.password) {
-				state.user = fakeUser;
-				state.isAuthenticated = true;
-			}
-		},
-		logout(state) {
-			state.user = null;
-			state.isAuthenticated = false;
-		},
-	},
+  name: 'user',
+  initialState: initialState,
+  reducers: {
+    login(state, action) {
+      if (
+        action.payload.email === fakeUser.email &&
+        action.payload.password === fakeUser.password
+      ) {
+        state.user = fakeUser;
+        state.isAuthenticated = true;
+      }
+    },
+    logout(state) {
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
 });
 export const { login, logout } = userSlice.actions;
 export default userSlice.reducer;
--- src/components/CartNav.jsx
+++ src/components/CartNav.jsx
@@ -5,36 +5,48 @@ import { useState } from 'react';
 import TrashIcon from './icons/TrashIcon';
 
 function CartNav() {
-	const { user } = useSelector((store) => store.user);
-	const [droped, setDroped] = useState(false);
-	return (
-		<div
-			className={styles.userIcons}
-			onMouseEnter={() => setDroped(true)}
-			onMouseLeave={() => setDroped(false)}>
-			<CartIcon />
-			{droped && (
-				<div className={styles.userMenu}>
-					<ul>
-						{user.cart.map((item) => (
-							<li key={item.id}>
-								<div>
-									<img src='/Rectangle157.jpg' />
-								</div>
-								<div className={styles.cartPrice}>
-									<span>{item.name}</span>
-									<span>{item.price}</span>
-								</div>
-								<div className={styles.cartIcon}>
-									<TrashIcon />
-								</div>
-							</li>
-						))}
-					</ul>
-				</div>
-			)}
-		</div>
-	);
+  const { user } = useSelector((store) => store.user);
+  const [droped, setDroped] = useState(false);
+  return (
+    <div
+      className={styles.userIcons}
+      onMouseEnter={() => setDroped(true)}
+      onMouseLeave={() => setDroped(false)}
+    >
+      <CartIcon />
+      {droped && (
+        <div className={styles.userMenu}>
+          <ul>
+            {user.cart.map((item) => (
+              <li key={item.id}>
+                <div>
+                  <div>
+                    <img src='/Rectangle157.jpg' />
+                  </div>
+                  <div className={styles.cartPrice}>
+                    <span>{item.name}</span>
+                    <span>
+                      {item.price}
+                      <span className={styles.priceDiscount}>
+                        {item.price + item.discount}
+                      </span>
+                    </span>
+                  </div>
+                </div>
+                <div className={styles.cartIcon}>
+                  <TrashIcon />
+                </div>
+              </li>
+            ))}
+            <div className={styles.cartBtns}>
+              <button>VIEW CART</button>
+              <button>CHECKOUT</button>
+            </div>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default CartNav;
