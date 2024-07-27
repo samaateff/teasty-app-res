884bc74d249d7fdc2cb48a7fdd309fd56bdfa574
Ghanduud <omar.ghandy780@gmail.com>
2024-07-27 05:56:26 +0300

    adding scss

--- src/components/CartNav.jsx
+++ src/components/CartNav.jsx
@@ -1,52 +1,52 @@
 import { useSelector } from 'react-redux';
-import CartIcon from './icons/CartIcon';
-import styles from './navbar.module.css';
 import { useState } from 'react';
+
+import CartIcon from './icons/CartIcon';
 import TrashIcon from './icons/TrashIcon';
 
+// import styles from './navbar.module.css';
+import styles from './cartl.module.scss';
+
 function CartNav() {
-  const { user } = useSelector((store) => store.user);
-  const [droped, setDroped] = useState(false);
-  return (
-    <div
-      className={styles.userIcons}
-      onMouseEnter={() => setDroped(true)}
-      onMouseLeave={() => setDroped(false)}
-    >
-      <CartIcon />
-      {droped && (
-        <div className={styles.userMenu}>
-          <ul>
-            {user.cart.map((item) => (
-              <li key={item.id}>
-                <div>
-                  <div>
-                    <img src='/Rectangle157.jpg' />
-                  </div>
-                  <div className={styles.cartPrice}>
-                    <span>{item.name}</span>
-                    <span>
-                      {item.price}
-                      <span className={styles.priceDiscount}>
-                        {item.price + item.discount}
-                      </span>
-                    </span>
-                  </div>
-                </div>
-                <div className={styles.cartIcon}>
-                  <TrashIcon />
-                </div>
-              </li>
-            ))}
-            <div className={styles.cartBtns}>
-              <button>VIEW CART</button>
-              <button>CHECKOUT</button>
-            </div>
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+	const { user } = useSelector((store) => store.user);
+	const [droped, setDroped] = useState(false);
+	return (
+		<div
+			className={`${styles.cartIcons}`}
+			onMouseEnter={() => setDroped(true)}
+			onMouseLeave={() => setDroped(false)}>
+			<CartIcon />
+			{droped && (
+				<div className={styles.cartMenu}>
+					<ul>
+						{user.cart.map((item) => (
+							<li key={item.id}>
+								<div>
+									<div>
+										<img src='/Rectangle157.jpg' />
+									</div>
+									<div className={styles.cartPrice}>
+										<span>{item.name}</span>
+										<span>
+											{item.price}
+											<span className={styles.priceDiscount}>{item.price + item.discount}</span>
+										</span>
+									</div>
+								</div>
+								<div>
+									<TrashIcon />
+								</div>
+							</li>
+						))}
+						<div className={styles.cartBtns}>
+							<button>VIEW CART</button>
+							<button>CHECKOUT</button>
+						</div>
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default CartNav;
