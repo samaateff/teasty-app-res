aadf3e0669bed1f688e144270d80008c3d8e410d
Ghanduud <omar.ghandy780@gmail.com>
2024-07-27 05:00:40 +0300

    finishin cart icon

--- src/components/CartNav.jsx
+++ src/components/CartNav.jsx
@@ -1,15 +1,40 @@
 import { useSelector } from 'react-redux';
 import CartIcon from './icons/CartIcon';
 import styles from './navbar.module.css';
-import store from '../Redux/store';
+import { useState } from 'react';
+import TrashIcon from './icons/TrashIcon';
 
 function CartNav() {
-  const { user } = useSelector((store) => store.user);
-  return (
-    <div className={styles.userIcons}>
-      <CartIcon />
-    </div>
-  );
+	const { user } = useSelector((store) => store.user);
+	const [droped, setDroped] = useState(false);
+	return (
+		<div
+			className={styles.userIcons}
+			onMouseEnter={() => setDroped(true)}
+			onMouseLeave={() => setDroped(false)}>
+			<CartIcon />
+			{droped && (
+				<div className={styles.userMenu}>
+					<ul>
+						{user.cart.map((item) => (
+							<li key={item.id}>
+								<div>
+									<img src='/Rectangle157.jpg' />
+								</div>
+								<div className={styles.cartPrice}>
+									<span>{item.name}</span>
+									<span>{item.price}</span>
+								</div>
+								<div className={styles.cartIcon}>
+									<TrashIcon />
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default CartNav;
