80a6948b82d925d4121391b536b39945c617dac9
Ghanduud <omar.ghandy780@gmail.com>
2024-07-28 04:47:54 +0300

    adding swipper

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import SliderOne from './SliderOne';
 
+import styles from './header.module.scss';
+
 function Header() {
-  return (
-    <div>
-      <SliderOne />
-    </div>
-  );
+	return (
+		<div className={styles.header}>
+			<SliderOne />
+		</div>
+	);
 }
 
 export default Header;
--- src/components/header/SliderOne.jsx
+++ src/components/header/SliderOne.jsx
@@ -1,74 +1,72 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './slider.module.scss';
 
 const food = [
-  {
-    desc: 'Lorem ipsum dolor sit amet, consec adipiscing, ipsum dolor sit amet, consec adipiscing consec .',
-    img: '/sliderImg1.png',
-    title: 'just grill it',
-  },
-  {
-    desc: 'Lorem ipsum dolor sit amet, consec adipiscing, ipsum dolor sit amet, consec adipiscing consec .',
-    img: '/sliderImg2.png',
-    title: 'out of oven',
-  },
-  {
-    desc: 'Lorem ipsum dolor sit amet, consec adipiscing, ipsum dolor sit amet, consec adipiscing consec .',
-    img: '/sliderImg1.png',
-    title: 'test two grill',
-  },
-  {
-    desc: 'Lorem ipsum dolor sit amet, consec adipiscing, ipsum dolor sit amet, consec adipiscing consec .',
-    img: '/sliderImg2.png',
-    title: 'test two pizza',
-  },
+	{
+		desc: 'Lorem ipsum dolor sit amet, consec adipiscing, ipsum dolor sit amet, consec adipiscing consec .',
+		img: '/sliderImg1.png',
+		title: 'just grill it',
+	},
+	{
+		desc: 'Lorem ipsum dolor sit amet, consec adipiscing, ipsum dolor sit amet, consec adipiscing consec .',
+		img: '/sliderImg2.png',
+		title: 'out of oven',
+	},
+	{
+		desc: 'Lorem ipsum dolor sit amet, consec adipiscing, ipsum dolor sit amet, consec adipiscing consec .',
+		img: '/sliderImg1.png',
+		title: 'test two grill',
+	},
+	{
+		desc: 'Lorem ipsum dolor sit amet, consec adipiscing, ipsum dolor sit amet, consec adipiscing consec .',
+		img: '/sliderImg2.png',
+		title: 'test two pizza',
+	},
 ];
-function SliderOne() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const slides = food;
 
-  function handleChangeSlides(index) {
-    setActiveIndex(index);
-  }
-  return (
-    <>
-      <div className={styles.sliderContainer}>
-        {slides.map((slide, index) => (
-          <div
-            key={index}
-            className={
-              activeIndex === index
-                ? `${styles.activeSlide} ${styles.slide}`
-                : `${styles.slide}`
-            }
-          >
-            <div>
-              <h3>{slide.title}</h3>
-              <p>{slide.desc}</p>
-            </div>
-            <img
-              src={slide.img}
-              alt={slide.title}
-              className={styles.sliderImg}
-              width={300}
-              height={300}
-            />
-          </div>
-        ))}
-      </div>
-      <div>
-        {slides.map((_, index) => (
-          <span
-            key={index}
-            className={activeIndex === index ? `${styles.activeBtn}` : ''}
-            onClick={() => handleChangeSlides(index)}
-          >
-            {index + 1}
-          </span>
-        ))}
-      </div>
-    </>
-  );
+function SliderOne() {
+	const [activeIndex, setActiveIndex] = useState(0);
+	const slides = food;
+
+	function handleChangeSlides(index) {
+		setActiveIndex(index);
+	}
+
+	useEffect(() => {
+		// if (activeIndex === slides.length - 1) return;
+
+		const timer = setTimeout(() => {
+			setActiveIndex((prevIndex) => (prevIndex + 1) % slides.length);
+		}, 5000);
+
+		return () => clearTimeout(timer);
+	}, [activeIndex, slides.length]);
+
+	return (
+		<div className={styles.sliderContainer}>
+			<div className={styles.slidsContainer}>
+				{slides.map((slide, index) => (
+					<div key={index} className={`${styles.slide} ${activeIndex === index ? styles.activeSlide : ''}`}>
+						<div>
+							<h3>{slide.title}</h3>
+							<p>{slide.desc}</p>
+						</div>
+						<img src={slide.img} alt={slide.title} className={styles.sliderImg} />
+					</div>
+				))}
+			</div>
+			<div className={styles.sliderButtons}>
+				{slides.map((_, index) => (
+					<span
+						key={index}
+						className={activeIndex === index ? styles.activeBtn : ''}
+						onClick={() => handleChangeSlides(index)}>
+						{index + 1}
+					</span>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default SliderOne;
